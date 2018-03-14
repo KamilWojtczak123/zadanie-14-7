ee904fff21f46a791defe7cc4214cf958421e47e
KamilWojtczak123 <35527613+KamilWojtczak123@users.noreply.github.com>
2018-03-14 17:10:15 +0100

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -5,7 +5,6 @@ var Contact = React.createClass({
     render: function() {
         return (
             <div className={'contactItem'}>
-                <img className={'contactImage'} src={'link-do-obrazka.png'}/>
                 <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
                 <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
                 <a href={'mailto:' + this.props.item.email}>
